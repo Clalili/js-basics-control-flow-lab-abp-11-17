f8fd5d41464f6b3533d6cac73c7be27486e5350b
Clara <c_c_456@hotmail.com>
2017-11-09 13:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ function ternaryCheckCity(city){
 }
 
 function switchOnCharmFromTip(tip){
-  case: 'generous':
-    return 'Thank you so much.';
-  case: 'not as generous':
-    return 'Thank you.';
-  default:
-  return 'Bye.';
-  
+  switch (tip){
+    case: 'generous':
+      return 'Thank you so much.';
+    case: 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 };
