e032f7bd263a845ab8536afa49d57e5243c79a1e
Clara <c_c_456@hotmail.com>
2017-11-09 13:39:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function scuberGreetingForFeet(distance){
   }
 };
 
-function ternaryCheckCity() {
-  let city;
-  city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+function ternaryCheckCity(city){
+  (city === 'NYC' ) ? "Ok, sounds good." : "No go.";
   return city;
 }
